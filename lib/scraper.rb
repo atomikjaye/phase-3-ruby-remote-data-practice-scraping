28c7e8ae15624c4e72924804dc19c65ad63f78ee
Jocelyn Naarden <tzionone@gmail.com>
2022-08-04 06:59:35 -0400

    Finished Assignment ✔

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+
+# p doc.css(".heading-financier").text.strip
+courses = doc.css(".card-centered-content .heading-35-semibold")
+
+courses.each do |course|
+  puts course.text.strip
+end
+
+puts courses[0].attributes
